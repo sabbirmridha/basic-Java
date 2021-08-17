aed547f68d5dc087b24410479d3923d50a1c56c9
sabbirmridha <sabbirmridha6@outlook.com>
2021-08-17 20:18:53 +0600

    Program for 1 to 100 using while loop has uploaded

---
+++ src/main/java/Practice/oneToHundredusingWhileLoop.java
@@ -0,0 +1,14 @@
+package Practice;
+// program for printing 1 to 100
+public class oneToHundredusingWhileLoop {
+    public static void main(String[] args) {
+        int n=1;  // initialization
+
+        while(n<=100) //condition
+        {
+            System.out.println(n);
+            n=n+1; // increment
+
+        }
+    }
+}
