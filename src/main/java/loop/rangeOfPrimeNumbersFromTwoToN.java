37bb3fd200a55aa0082e48771e3a8beee689d8f0
sabbirmridha <sabbirmridha6@outlook.com>
2021-08-20 11:13:01 +0600

    prime numbers program updated

--- src/main/java/loop/rangeOfPrimeNumbersFromTwoToN.java
+++ src/main/java/loop/rangeOfPrimeNumbersFromTwoToN.java
@@ -4,38 +4,34 @@ import static java.lang.Math.sqrt;
 // program to show range of prime numbers from 2 to n using while loop where n is the number by user input
 public class rangeOfPrimeNumbersFromTwoToN {
     public static void main(String[] args) {
-        int begin =2;
-        int num1 , cal ,  num2 ;
-        int flag;
 
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter the value for n:");
-         num1= input.nextInt();
+        System.out.println("Enter the value for n :");
+        int num1= input.nextInt();
+        int count =0;
+        int k=2;
 
-        while(begin<=num1){
-
-           num2= (int)sqrt(begin);
-
-            cal=2;
-            flag= 1;
-
-            while(cal<=num2){
-
-                if(begin%cal == 0)
-                {
-                    flag=0;
+        while(k<=num1){
+            boolean flag =true;
+            for(int i=2;i<=k/2;i++){
+                if(k%i==0){
+                    flag=false;
                     break;
                 }
-                cal++;
             }
-            begin++;
 
-            if(flag){
-                System.out.println(begin);
-                begin++;
+            if(flag==true){
+                System.out.println(k);
+                count++ ;
             }
+            k++;
 
         }
+        System.out.println("total prime numbers from 2 to  "+num1+"  is   "+count );
+
+
+
+
 
 
 
